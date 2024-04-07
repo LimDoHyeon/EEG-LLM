37f1b6ebebe5ea189eafcb93690f6663da4b925c
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-04-07 15:17:05 +0900

    [Refactoring] architecture

---
+++ TreeOfThought/bfs.py
@@ -0,0 +1,30 @@
+import itertools
+import numpy as np
+from functools import partial
+from gpt import gpt
+
+def get_value(task, x, y, n_evaluate_sample, cache_value=True):
+    pass
+
+
+def get_values(task, x, ys, n_evaluate_sample, cache_value=True):
+    pass
+
+
+def get_votes(task, x, ys, n_evaluate_sample):
+    pass
+
+
+def get_proposals(task, x, y):
+    pass
+
+
+def get_samples(task, x, y, n_generate_sample, prompt_sample, stop):
+    pass
+
+
+def solve(args, task, idx, to_print=True):
+    pass
+
+# def naive_solve(args, task, idx, to_print=True):
+#    pass
