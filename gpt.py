8cc92ec727ffb09d2bee149be0550587d6429d80
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-04-14 20:38:35 +0900

    [feat]Tree of Thought - demo

---
+++ gpt.py
@@ -0,0 +1,9 @@
+import argparse
+from TreeOfThought.src.tot.methods.bfs import solve
+from TreeOfThought.src.tot.tasks.game24 import Game24Task
+
+args = argparse.Namespace(backend='gpt-4', temperature=0.7, task='game24', naive_run=False, prompt_sample=None, method_generate='propose', method_evaluate='value', method_select='greedy', n_generate_sample=1, n_evaluate_sample=3, n_select_sample=5)
+
+task = Game24Task()
+ys, infos = solve(args, task, 900)
+print(ys[0])
