662276d4e9fdcc6b50b1aac78e9b9e76a9d4253c
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-04-28 14:44:53 +0900

    [feat] Decision Tree for thinking

---
+++ DecisionTree/dt.py
@@ -0,0 +1,3 @@
+# In EEG-GPT project, Tree of Thought method is not used in actual.
+# Instead, the Decision Tree which similar to Tree of Thought is used.
+# So, after considerable thinking about which step GPT should take, DT will be implemented.
