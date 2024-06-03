12c7adf334e744aedcd93bdd2aa984ec8c1e45cb
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-06-03 11:33:45 +0900

    [Refactor] Features&Fisher ratio 한 파일에서 작업 가능하도록 수정
    #7

--- preprocessing/feature_extraction.py
+++ preprocessing/feature_extraction.py
@@ -7,31 +7,28 @@ Additional filtering is not required as the data is already preprocessed.
 """
 
 
-def load_eeg_data(file_path, sfreq=250):
+def load_eeg_data(file_path):
     data = pd.read_csv(file_path)
-    eeg_data = data.values  # 모든 열을 가져옴
-    ch_names = data.columns.tolist()  # 열 이름을 채널 이름으로 사용
-
-    # MNE의 Info 객체 생성
-    info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types='eeg')
-    # RawArray 객체 생성
-    raw = mne.io.RawArray(eeg_data.T, info)
-
-    return raw
+    return data
 
 
 def compute_band_power(raw, band):
     fmin, fmax = band  # 주파수 대역 설정
     data = raw.get_data()
     sfreq = raw.info['sfreq']
-    psds, freqs = mne.time_frequency.psd_array_welch(data, sfreq=sfreq, fmin=fmin, fmax=fmax,
-                                                     n_fft=2048)  # 전력 스펙트럼 밀도 계산
+    data = data[:-1, :]  # 마지막 열은 라벨이므로 제외
+    psds, freqs = mne.time_frequency.psd_array_welch(data, sfreq=sfreq, fmin=fmin, fmax=fmax, n_fft=2048)  # 전력 스펙트럼 밀도 계산
     # 주파수 대역의 전력 계산
     band_power = np.sum(psds, axis=-1)
     return band_power
 
 
-def extract_features(raw):
+def extract_features(data, sfreq=250):
+    eeg_data = data.values  # 모든 열을 가져옴
+    ch_names = data.columns.tolist()  # 열 이름을 채널 이름으로 사용
+    info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types='eeg')  # MNE의 Info 객체 생성
+    raw = mne.io.RawArray(eeg_data.T, info)  # RawArray 객체 생성
+
     # 주파수 대역 정의
     delta_band = (0.5, 4)
     theta_band = (4, 8)
