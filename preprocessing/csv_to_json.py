59e2faa7d73b2b5f593480b1c04b34df95a705c4
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-07-27 14:01:46 +0900

    [Feat] 라벨 미반환 json 변환기
    #10

--- preprocessing/csv_to_json.py
+++ preprocessing/csv_to_json.py
@@ -5,7 +5,7 @@ csv를 json으로, json을 jsonl로 변환하기 위한 베이스 함수.
 import pandas as pd
 import numpy as np
 import json
-import feature_extraction
+import Preprocessing.feature_extraction
 
 
 def csv_to_json(df, window_size, selected_columns, labels):
@@ -45,7 +45,7 @@ def csv_to_json(df, window_size, selected_columns, labels):
             features_str += f"{key}:\n"
             features_str += "\n".join([f"  {v}" for v in value])
             features_str += "\n"
-        combined_prompt = f"{prompt}\n{features_str}"  # 실제 줄바꿈 적용
+        combined_prompt = f"{prompt}\n{features_str}"
 
         json_entry = {
             "prompt": combined_prompt,  # 끝에 불필요한 줄바꿈 제거
@@ -53,6 +53,37 @@ def csv_to_json(df, window_size, selected_columns, labels):
         }
 
         json_array.append(json_entry)
+
+    return json_array
+
+
+def csv_to_json_without_label(df, window_size, selected_columns):
+    json_array = []
+
+    for start in range(0, len(df) - window_size + 1, window_size):
+        window_data = df.iloc[start:start + window_size, selected_columns]  # DataFrame으로 윈도우 데이터 선택
+
+        features = Preprocessing.feature_extraction.extract_features(window_data, list(range(len(selected_columns))))  # 인덱스를 사용하여 피처 추출
+        features_dict = features.to_dict('index')  # DataFrame을 딕셔너리 형태로 변환
+
+        # 새로운 형식의 features_dict_with_keys 생성
+        features_dict_with_keys = {
+            f"at channel {selected_columns[i]}": [
+                f"Alpha:Delta Power Ratio: {features_dict[i]['Alpha:Delta Power Ratio']}",
+                f"Theta:Alpha Power Ratio: {features_dict[i]['Theta:Alpha Power Ratio']}",
+                f"Delta:Theta Power Ratio: {features_dict[i]['Delta:Theta Power Ratio']}"
+            ] for i in range(len(selected_columns))
+        }
+
+        prompt = f"Quantitative EEG: In a {window_size / 250} second period,"
+        features_str = ""
+        for key, value in features_dict_with_keys.items():
+            features_str += f"{key}:\n"
+            features_str += "\n".join([f"  {v}" for v in value])
+            features_str += "\n"
+        combined_prompt = f"{prompt}\n{features_str}"
+
+        json_array.append(combined_prompt)
 
     return json_array
 
