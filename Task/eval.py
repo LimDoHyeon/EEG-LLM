761a75d37da2c2d6af3d5bfd09323e46ad1c6b87
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-07-28 13:21:29 +0900

    [Feat] update eval function
    #10
    - Inference based on GPT model and get performance metrics below:
      - Accuracy
      - F1 score
      - Kohen's kappa

--- Task/eval.py
+++ Task/eval.py
@@ -4,28 +4,43 @@ metrics : Kappa's coefficient and F1 Score
 """
 
 from openai import OpenAI
-import numpy as np
-import pandas as pd
-from sklearn.metrics import cohen_kappa_score, f1_score
+from sklearn.metrics import cohen_kappa_score, f1_score, accuracy_score
+from csv_to_json_4o import csv_to_json_without_label
 
 
 def use_model(msg):
-    response = client.chat.completions.create(
-        model="ft:davinci-002:personal::9nzVtmd3",
-        messages=[],  # [] 자리에 msg 들어가야 함
-        temperature=1,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+    completion = client.chat.completions.create(
+        model="ft:gpt-4o-mini:my-org:custom_suffix:id",
+        messages=msg
     )
-    return response.choices[0].message
+    return completion.choices[0].message
 
-def eval():
+
+def evaluate(data, label, window_size, selected_columns):
     """
-    1. test 데이터(csv) 로드
+    1. test 데이터(csv) 파라미터로 받음
     2. 전처리 후 json 형태로 변환하여, 한 task씩 gpt에 입력
     3. gpt의 completion을 버퍼에 저장
     4. 버퍼의 completion을 모아서 실제 라벨과 함께 F1 Score, Kappa Coefficient에 입력
     5. 결과 출력
     """
+    model_pred = []
+
+    # Get responses(prediction) from the model
+    json_data = csv_to_json_without_label(data, window_size, selected_columns)
+    for i in range(len(json_data)):
+        response = use_model(json_data[i])
+        model_pred.append(response)
+
+    # Convert the model prediction and label to integer
+    model_pred = [int(pred) for pred in model_pred]
+    label = [int(lab) for lab in label]
+
+    # Calculate Accuracy, F1 Score, Kappa Coefficient
+    accuracy = accuracy_score(label, model_pred)
+    f1 = f1_score(label, model_pred, average='weighted')
+    kappa = cohen_kappa_score(label, model_pred)
+
+    print('Accuracy : {0:.4f}'.format(accuracy))
+    print('F1 Score : {0:.4f}'.format(f1))
+    print('Kappa Coefficient : {0:.4f}'.format(kappa))
