79ca9037601247264c5e7ac85c96108f091f5612
ewofix <addorothy00@naver.com>
2024-09-05 18:42:08 +0900

    [Fix] data_split_chunk.py code correction
    Remove downsampling

--- Utils/Selection/data_split_chunk.py
+++ Utils/Selection/data_split_chunk.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Setting
 n_channels = 9
-sampling_freq = 250  # Original sampling frequency
+sampling_freq = 250     # in Hertz
 ch_names = [
     'FC3', 'FCz', 'FC4',
     'C3', 'Cz', 'C4',
@@ -10,12 +10,11 @@ ch_names = [
 ]
 ch_types = ['eeg'] * n_channels
 
-def preprocess_data(filepaths, new_sampling_freq=100):
+def preprocess_data(filepaths):
     """
-    Preprocess EEG data by downsampling and then chunking.
-
+    Preprocess EEG data: Load and transpose.
+    
     :param filepaths: List of file paths for CSV files
-    :param new_sampling_freq: New sampling frequency after downsampling
     :return: Dictionary with preprocessed DataFrames
     """
     eeg_raw = {}
@@ -23,23 +22,16 @@ def preprocess_data(filepaths, new_sampling_freq=100):
     for i in range(len(filepaths)):
         temp_pd = pd.read_csv(filepaths[i])
         temp_pd = temp_pd.drop(columns=['label'])
-        
-        # Transpose the data so channels are rows
+
         temp_pd = temp_pd.transpose()
-        
-        # Downsample
-        num_samples = temp_pd.shape[1]
-        downsample_factor = int(sampling_freq / new_sampling_freq)
-        downsampled_pd = temp_pd.iloc[:, ::downsample_factor]
-        
-        eeg_raw[i] = downsampled_pd
-    
+        eeg_raw[i] = temp_pd
+
     return eeg_raw
 
-def split_columns(df, chunk_size=400):
+def split_columns(df, chunk_size=1000):
     """
     Split DataFrame into chunks of specified column size.
-
+    
     :param df: DataFrame to split
     :param chunk_size: Number of columns per chunk
     :return: List of DataFrames split into chunks
@@ -51,10 +43,10 @@ def split_columns(df, chunk_size=400):
         df_list.append(df_chunk)
     return df_list
 
-def split_data_into_chunks(eeg_raw, chunk_size=400):
+def split_data_into_chunks(eeg_raw, chunk_size=1000):
     """
     Split the preprocessed EEG data into chunks.
-
+    
     :param eeg_raw: Dictionary with preprocessed DataFrames
     :param chunk_size: Number of columns per chunk
     :return: Dictionary with chunked DataFrames
